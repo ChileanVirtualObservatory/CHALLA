ebd3823151e8cdd4c34d505ad2319d47653ab53a
mavillan <mavillan@alumnos.inf.utfsm.cl>
2015-06-02 03:51:41 -0300

    algunos fixes menores

---
+++ tools/fellWalker_test.py
@@ -0,0 +1,26 @@
+import chivolib.workspace as ws
+import chivolib.fellWalker as fw
+import numpy as np
+import timeit
+from chivolib.spectral import Cube
+
+global M
+global model
+
+ws.import_file("fits/combined-278000.fits")
+#ws.import_file("fits/calibrated.ms.image.spectrum.J113740.6-010454.spw0.image.fits")
+#ws.import_file("fits/calibrated.ms.line.spw0.source15.image.fits")
+#ws.import_file("fits/Boom.cm.cln.fits")
+
+elm=ws.elements()
+#print elm
+ndd=elm['combined-278000-0']
+#ndd=elm['calibrated.ms.image.spectrum.J113740.6-010454.spw0.image-0']
+#ndd=elm['calibrated.ms.line.spw0.source15.image-0']
+#ndd=elm['Boom.cm.cln-0']
+if ndd.meta['NAXIS']==4:
+   cb=Cube(ndd.data[0],ndd.meta)
+else:
+   cb=Cube(ndd.data,ndd.meta)
+caa=fw.fellWalker(cb)
+print caa	
