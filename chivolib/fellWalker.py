3347b484c65641d5d449772546826871737dc134
mavillan <mavillan@alumnos.inf.utfsm.cl>
2015-06-09 00:11:35 -0300

    merge function completed and working

--- chivolib/fellWalker.py
+++ chivolib/fellWalker.py
@@ -13,7 +13,7 @@ def get_params():
 
 
 def create_caa(data):
-   caa=np.zeros_like(data)
+   caa=np.zeros_like(data).astype(int)
    #Check invalid pixels (below threshold)
    shape=caa.shape
    rms=compute_rms(data)
@@ -32,10 +32,6 @@ def compute_rms(data):
    res=data[data < 0]
    fin=(res*res).sum()/len(res)
    return np.sqrt(fin)
-       
-
-def check_merge():
-   return
 
 
 def max_gradient(pos,data,caa):
@@ -87,6 +83,36 @@ def verify_peak(pos,data,caa):
                max_val=data[neigh]
    return max_pos
 
+def check_merge(clumpId,clump,data,caa):
+   shape=data.shape
+   max_clump=0 #max value in clump
+   max_border=0 #max value in border
+   neighId=-1
+   for pos in clump[clumpId]:
+      #First, find the peak of clump
+      if data[pos]>max_clump:
+         max_clump=data[pos]
+      #Second, verify if it is a border clump pixel
+      for i in range(-1,2):
+         for j in range(-1,2):
+            for k in range(-1,2):
+               neigh=(pos[0]+i,pos[1]+j,pos[2]+k) #position of neighbour point
+               if i==j==k==0:
+                  #don't check itself
+                  continue
+               elif neigh[0]<0 or neigh[1]<0 or neigh[2]<0 or neigh[0]>=shape[0] or neigh[1]>=shape[1] or neigh[2]>=shape[2]:
+                  #don't go outside of cube
+                  continue
+               elif caa[neigh]>0 and caa[neigh]!=clumpId:
+                  #border clump pixel found!
+                  #Verify if it is the highest
+                  border_val=(data[pos]+data[neigh])/2. #value at border
+                  if border_val>max_border:
+                     max_border=border_val
+                     neighId=caa[neigh]
+   return (max_clump-max_border,neighId)
+
+
 def walkup(pos,path,data,caa):
    next_pos=max_gradient(pos,data,caa)
 
@@ -130,7 +156,7 @@ def fellWalker(orig_cube):
             pos=(i,j,k)
             path.append(pos)
             path=walkup(pos,path,data,caa)
-            print "nueva path:",path
+            print "new path:",path
 
             if caa[path[-1]]>0:
                #Ascent path reach an existing path
@@ -165,16 +191,37 @@ def fellWalker(orig_cube):
       elif deleted:
          #If deleted have any index
          clump[deleted[0]]=clump.pop(clumpId)
+         #update caa
+         for pos in clump[deleted[0]]:
+            caa[pos]=deleted[0]
          del deleted[0]
          deleted.append(clumpId)
 
 
-
    ####refine 2, merging clumps
+   minDip=get_params()['minDip']
+   merge=True
+   while merge:
+      #Don't iterate again unless some merge occur
+      merge=False
+      for clumpId in clump:
+         dip,neighId=check_merge(clumpId,clump,data,caa)
+         if neighId!=-1 and dip<=minDip:
+            #Merge it!
+            merge=True
+            break #dictionaries can't change size while iterating it
+      
+      if merge:
+         #Update caa
+         for pos in clump[neighId]:
+            caa[pos]=clumpId
+         #Merge pixels
+         clump[clumpId]+=(clump.pop(neighId))
+         print "Merged clumps {0} and {1}".format(clumpId,neighId)
+
 
    ####some statistics
-   print len(clump)
-   for clumpId in clump:
-      print clumpId
+   print "Number of clumps:",len(clump)
+
 
    return caa,clump
