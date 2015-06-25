a3c1570abee9b36f370603dcbbaef2968cc444e2
mavillan <mavillan@alumnos.inf.utfsm.cl>
2015-06-25 03:22:19 -0300

    bbox function added

--- chivolib/bbox.py
+++ chivolib/bbox.py
@@ -3,11 +3,31 @@ import sympy as sp
 import pickle
 
 """
-bbox funtion return the bounding box of an ellipsoid 
+bbox function return the bounding box of an ellipsoid 
 of the form (x-u)^T.A.(x-u)=k.
+It returns (xinf,xsup,yinf,ysup,zinf,zsup)
 """
+def lamb(func):
+    return sp.lambdify((a,b,c,d,e,f,g,h,i), func, modules='numpy')
 
 def bbox(A,k):
-	f=file("bbox.pickle","o")
-	functions=pickle.load(f)
-	f.close()
+	###Load bounding functions
+	fl=file("bbox.pickle","o")
+	expr=pickle.load(fl)
+	fl.close()
+
+	###Transform Sympy expressions to Numpy evaluated functions
+	functions=map(lamb,expr)
+
+	###Applying functions
+	args=A.reshape(1,9)
+	(a,b,c,d,e,f,g,h,i)=tuple(args[0])
+	box=list()
+	for f in functions:
+		box.append(f(a,b,c,d,e,f,g,h,i))
+
+	###Ordering
+	ret=np.min(box[0],box[1]),np.max(box[0],box[1]),
+		np.min(box[2],box[3]),np.max(box[2],box[3]),
+		np.min(box[4],box[5]),np.max(box[4],box[5]))
+	return ret
--- chivolib/bbox_script.py
+++ chivolib/bbox_script.py
@@ -9,11 +9,8 @@ Because we only want to know the dimensions of the bounding
 box, i.e, displacement from center point, we center the ellipsoid
 at u=(0,0,0) for simplicity.
 It generates a file called "bbox.pickle" that serialize a tuple
-of functions that gerate the box limits.
+of expressions for the box limits.
 """
-
-def lamb(func):
-    return sp.lambdify((a,b,c,d,e,f,g,h,i), func, modules='numpy')
 
 (a,b,c,d,e,f,g,h,i)=sp.symbols('a b c d e f g h i')
 (x,y,z)=sp.symbols('x y z')
@@ -25,25 +22,24 @@ eqx=sp.diff(eq,x)
 eqy=sp.diff(eq,y)
 eqz=sp.diff(eq,z)
 
-###Solve for all bounds ###
+###Solve for all bounds 
 solx=sp.solve([eq,eqy,eqz],[x,y,z])
 soly=sp.solve([eq,eqx,eqz],[x,y,z])
 solz=sp.solve([eq,eqx,eqy],[x,y,z])
 
-###Simplify the bounds ###
+###Simplify the bounds 
 x1=sp.simplify(solx[0][0])
 x2=sp.simplify(solx[1][0])
 y1=sp.simplify(soly[0][1])
 y2=sp.simplify(soly[1][1])
 z1=sp.simplify(solz[0][2])
 z2=sp.simplify(solz[1][2])
+obj=(x1,x2,y1,y2,z1,z2)
 
-###Convert symbolic expressions to functions evaluated with numpy###
-x1,x2,y1,y2,z1,z2=map(lamb,(x1,x2,y1,y2,z1,z2))
-functions=(x1,x2,y1,y2,z1,z2)
-
-###Serializing...###
+###Serializing...
 f=file("bbox.pickle","w")
-pickle.dump(functions,f)
+pickle.dump(obj,f)
 f.close()
+
+
 
